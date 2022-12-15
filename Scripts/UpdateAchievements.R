9971247ecb5626d8d7814720b81c2a69342a3f30
aanil3 <Abinav_anil@hotmail.com>
2022-12-14 22:54:52 -0500

    Pushing achievement changes

---
+++ Scripts/UpdateAchievements.R
@@ -0,0 +1,14 @@
+require(DBI)
+require(dbplyr)
+require(RSQLite)
+require(stringr)
+
+con <- dbConnect(SQLite(), "database/SHLHistory.db")
+dbConnect(SQLite(), "database/SHLHistory")
+
+achieve <- read.csv("achievement.csv")
+
+dbWriteTable(con, "achievement", achieve, overwrite =F, append = T, row.names=FALSE)
+
+AllPlayers <- dbGetQuery(con, "SELECT FHMID FROM shlCareerRegularSeason")
+
